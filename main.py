68002f4d3fcf6bebed5aeeeebee513ea4919e35a
mharkness1 <mharkness.uk@gmail.com>
2025-01-20 22:50:24 +0000

    Creating sundry files

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	clock = pygame.time.Clock()
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.surface,fill((0,0,0))
+			
+		screen.fill("black")
 		pygame.display.flip()
+		player.draw(screen)
+
+		#limit the framerate 
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
